302b1cb7cbd55c28b4958ac414d186debb4ae407
aozcann <ozcanahmet94@gmail.com>
2022-01-24 14:15:23 +0300

    list-comprehensions

---
+++ list-compr.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    i = x+1
+    j = y+1
+    k = z+1
+    result =[]
+    result = [[x,y,z] for x in range(0,i) for y in range(0,j) for z in range(0,k) if (x + y + z != n) ]
+    print(result)
